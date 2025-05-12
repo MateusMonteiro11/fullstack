aacec2785a156fc981186d8130668947e389b8f8
MateusMonteiro11 <mateusinaciomonteiro519@gmail.com>
2025-05-12 10:25:32 -0300

    aula 07 finalizada

--- aula07/servidor.js
+++ aula07/servidor.js
@@ -14,29 +14,3 @@ var server = http.createServer (app);
 server.listen (80);
 
 console.log('Servidor rodando'.rainbow)
-
-app.get ('/', function (requisicao, resposta) {
-resposta.redirect ('home.html')
-
-})
-
-app.get ('/inicio', function (requisicao, resposta) {
-var nome = requisicao.query.info;
-console.log(nome) ;
-
-})
-
-app.post('/inicio', function (requisicao, resposta) {
-    var data = requisicao.body.data;
-    console.log (data);
-
-})
-
-app.get('/cadastro', function (requisicao, resposta) {
-    var nome = requisicao.query.nome;
-    var sobrenome = requisicao.query.sobrenome;
-    var nascimento = requisicao.query.nascimento;
-    var civil = requisicao.query.civil;
-
-    resposta.render('resposta_cadastro', { nome, sobrenome, nascimento, civil });
-});
