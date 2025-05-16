669175e5b55ebca000c15ad9f376824e71df72da
MateusMonteiro11 <mateusinaciomonteiro519@gmail.com>
2025-05-16 20:55:21 -0300

    algumas correções

--- aula04/advinhação/java.js
+++ aula04/advinhação/java.js
@@ -1,7 +1,7 @@
 const randomNumber = Math.floor(Math.random() * 100);
 const guessButton = document.getElementById('guessButton');
 const guessInput = document.getElementById('guessInput');
-const message = document.getElementById('message');
+const message = document.getElementById('message')
 
 guessButton.addEventListener('click', () => {
     const userGuess = parseInt(guessInput.value);
@@ -9,11 +9,14 @@ guessButton.addEventListener('click', () => {
     if (isNaN(userGuess) || userGuess < 0 || userGuess > 99) {
         message.textContent = 'Por favor, insira um número válido entre 0 e 99.';
     } else if (userGuess < randomNumber) {
-        message.textContent = 'Muito baixo! Tente novamente.';
+        message.innerText= 'Muito baixo! Tente novamente.';
+        message.style.backgroundColor = 'red'
     } else if (userGuess > randomNumber) {
-        message.textContent = 'Muito alto! Tente novamente.';
+        message.innerText = 'Muito alto! Tente novamente.';
+        message.style.backgroundColor = 'red'
     } else {
-        message.textContent = 'Parabéns! Você acertou o número!';
+        message.innerText = 'Parabéns! Você acertou o número!';
+        message.style.backgroundColor = 'green'
     }
 
     guessInput.value = '';
--- aula06/java.js
+++ aula06/java.js
@@ -15,28 +15,42 @@ bola.img.onload = function () {
 };
 
 bola.desenha = function (angulo) {
-    ctx.save(); 
-    ctx.translate(this.x, this.y); 
-    ctx.rotate(angulo); 
-    ctx.drawImage(this.img, -this.raio, -this.raio, 2 * this.raio, 2 * this.raio); 
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(angulo);
+    ctx.drawImage(this.img, -this.raio, -this.raio, 2 * this.raio, 2 * this.raio);
     ctx.restore();
 };
 
 function animacao() {
-    ctx.clearRect(0, 0, 600, 600);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     let angulo = Math.atan2(bola.y - mouseY, bola.x - mouseX);
     bola.desenha(angulo);
     requestAnimationFrame(animacao);
-};
+}
 
 let mouseX = 0;
 let mouseY = 0;
 
-document.addEventListener('mousemove', function (evento) {
+canvas.addEventListener('mousemove', function (evento) {
     let rect = canvas.getBoundingClientRect();
     mouseX = evento.clientX - rect.left;
     mouseY = evento.clientY - rect.top;
 
     bola.x = mouseX;
     bola.y = mouseY;
+
+    if (bola.x < bola.raio) {
+        bola.x = bola.raio;
+    }
+    if (bola.x > canvas.width - bola.raio) {
+        bola.x = canvas.width - bola.raio;
+    }
+
+    if (bola.y < bola.raio) {
+        bola.y = bola.raio;
+    }
+    if (bola.y > canvas.height - bola.raio) {
+        bola.y = canvas.height - bola.raio;
+    }
 });
